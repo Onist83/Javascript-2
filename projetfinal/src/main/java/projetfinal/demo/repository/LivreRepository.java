5ed26da13be59da491e8697f33a0014647d2db1f
Tanguy <t.stineau@outlook.fr>
2026-01-20 15:10:39 +0100

    Delete projetfinal directory

--- projetfinal/src/main/java/projetfinal/demo/repository/LivreRepository.java
+++
@@ -1,9 +0,0 @@
-package projetfinal.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import projetfinal.demo.model.Livre;
-
-import java.util.List;
-
-public interface LivreRepository extends JpaRepository<Livre, Long> {
-}
--- projetfinal/src/main/java/projetfinal/demo/repository/MembreRepository.java
+++
@@ -1,8 +0,0 @@
-package projetfinal.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import projetfinal.demo.model.Membre;
-
-public interface MembreRepository extends JpaRepository<Membre, Long> {
-
-}
